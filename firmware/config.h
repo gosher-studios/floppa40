3b854aeb4bff624908c963ed7726512192e38a3c
chxry <chxry@floppa.world>
2022-12-14 22:50:27 +0000

    firm

--- firmware/config.h
+++ firmware/config.h
@@ -1,4 +1,26 @@
+// (c) gosher studios 2022
 #pragma once
+#include "config_common.h"
 
-// https://github.com/qmk/qmk_firmware/tree/0a3f7e48690bb2b7b008300a54554979a55be19a/keyboards/ferris/sweep
-// use this as a reference
+// info
+#define PRODUCT "Floppa40"
+#define MANUFACTURER "Gosher Studios"
+#define DEVICE_VER 0x0100
+#define PRODUCT_ID 0x0000
+#define VENDOR_ID 0x0000
+
+// pins
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+#define DIRECT_PINS { \
+    { GP2, GP6, GP29, GP27, GP22, GP23 }, \
+    { GP3, GP7, GP12, GP14, GP16, GP21 }, \
+    { GP4, GP8, GP28, GP26, GP20, NO_PIN }, \
+    { GP5, GP9, GP13, GP15, NO_PIN, NO_PIN }, \
+}
+#define DIRECT_PINS_RIGHT { \
+    { GP4, GP7, GP12, GP27, GP21, GP20 }, \
+    { GP5, GP29, GP28, GP15, GP23, NO_PIN }, \
+    { GP6, GP8, GP13, GP14, GP26, GP22 }, \
+    { GP9, GP16, NO_PIN, NO_PIN, NO_PIN, NO_PIN }, \
+}
